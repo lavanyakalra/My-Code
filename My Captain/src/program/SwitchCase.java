68428d64a4160bf41d0ae9c230380d30e14c04e3
Lavanya Kalra <Lavanya Kalra@192.168.1.6>
2020-07-16 13:34:04 +0530

    commit done

---
+++ My Captain/src/program/SwitchCase.java
@@ -0,0 +1,24 @@
+package program;
+
+import java.util.Scanner;
+
+public class SwitchCase {
+public static void main(String[] args) {
+	
+	int choice;
+		System.out.println("Pick one : Hi \t2 . Hey \t3. hello\t");
+		Scanner s =new Scanner (System.in);
+		choice= s.nextInt();
+		switch(choice)
+		{
+		case 1 : System.out.println("Youn said Hi");
+		         break;
+		case 2: System.out.println("You said Hey");
+		         break;
+		case 3: System.out.println("You said Hello"); 
+		        break;
+		default: System.out.println("Invalid Choice");
+		
+		}
+     }
+}
